0a4cadb0dd140f74190d4ed504c54ef483920b46
mortezaa <mortezajafari76@gmail.com>
2025-09-15 17:29:53 +0330

    bugfix: routes issue

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Mortezaa97\Regions\Http\Controllers\CountyController;
+use Mortezaa97\Regions\Http\Controllers\ProvinceController;
 
 Route::prefix('api/counties')->group(function () {
     Route::get('/', [CountyController::class, 'index'])->name('counties.index');
     Route::get('/index/{province?}/{county?}', [CountyController::class, 'index'])->name('counties.province');
     Route::get('/{county}', [CountyController::class, 'show'])->name('counties.show');
+});
+
+Route::prefix('api/provinces')->group(function () {
+    Route::get('/', [ProvinceController::class, 'index'])->name('provinces.index');
+    Route::get('/{province}', [ProvinceController::class, 'show'])->name('provinces.show');
 });
