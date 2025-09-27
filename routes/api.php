aea3137cc1868b1398dec804697ab09ab0baf644
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:37 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Route;
 use Mortezaa97\Regions\Http\Controllers\CountyController;
 use Mortezaa97\Regions\Http\Controllers\ProvinceController;
 
-Route::prefix('api/counties')->group(function () {
+Route::prefix('api/counties')->middleware('api')->group(function () {
     Route::get('/', [CountyController::class, 'index'])->name('counties.index');
     Route::get('/index/{province?}/{county?}', [CountyController::class, 'index'])->name('counties.province');
     Route::get('/{county}', [CountyController::class, 'show'])->name('counties.show');
 });
 
-Route::prefix('api/provinces')->group(function () {
+Route::prefix('api/provinces')->middleware('api')->group(function () {
     Route::get('/', [ProvinceController::class, 'index'])->name('provinces.index');
     Route::get('/{province}', [ProvinceController::class, 'show'])->name('provinces.show');
 });
