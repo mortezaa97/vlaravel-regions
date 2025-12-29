903a9af03bc22345b4dc38755a1078dce318665c
mortezaa <mortezajafari76@gmail.com>
2025-12-29 03:50:14 +0330

    fix user model namespace

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mortezaa97\Regions\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
